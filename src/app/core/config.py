bc33c7e58e1596d1b9d55dc032578c7cd966af39
VictorIbs1307 <voli@hotmail.dk>
2023-11-05 18:18:08 +0100

    Add dotenv

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -1,17 +1,23 @@
 import os
+# from dotenv import load_dotenv
+
+
 
 class Settings:
-    PROJECT_NAME:str = "My Application"
-    PROJECT_VERSION: str = "V1.0.0"
+    def __init__(self):
+        # load_dotenv()
+        
+        PROJECT_NAME:str = "My Application"
+        PROJECT_VERSION: str = "V1.0.0"
 
-    POSTGRES_USER : str = os.getenv("POSTGRES_USER", "postgres")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
-    POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
-    POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432) # default postgres port is 5432
-    POSTGRES_DB : str = os.getenv("POSTGRES_DB","my_db")
-    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+        POSTGRES_USER : str = os.getenv("POSTGRES_USER", "postgres")
+        POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
+        POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
+        POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432) # default postgres port is 5432
+        POSTGRES_DB : str = os.getenv("POSTGRES_DB","my_db")
+        self.database_url = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
     def gino_config(self):
         return {
-            "dsn": self.DATABASE_URL   
+            "dsn": self.database_url   
         }
